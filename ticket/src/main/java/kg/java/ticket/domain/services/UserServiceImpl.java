a142be16e28849fe9a214da194aa1062f3cb9773
User <stparchkg@gmail.com>
2023-11-20 21:07:00 +0600

    change constructor for UserService

--- ticket/src/main/java/kg/java/ticket/domain/services/UserServiceImpl.java
+++ ticket/src/main/java/kg/java/ticket/domain/services/UserServiceImpl.java
@@ -17,8 +17,9 @@ public class UserServiceImpl implements UserService {
     private final UserRepository userRepository;
     private final UserMapper userMapper;
 
-    public UserServiceImpl(UserRepository userRepository) {
+    public UserServiceImpl(UserRepository userRepository,UserMapper userMapper) {
         this.userRepository = userRepository;
+        this.userMapper = userMapper;
     }
 
     @Override
